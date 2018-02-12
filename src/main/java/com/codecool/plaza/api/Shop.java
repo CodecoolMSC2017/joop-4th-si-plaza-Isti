92a8bce68df45bf41a3d90bdf48402f22aa2138c
Gyorfisti <gyorfisti@gmail.com>
2018-02-12 15:19:44 +0100

    finished Shop interface, added 3 more exceptions

--- src/main/java/com/codecool/plaza/api/Shop.java
+++ src/main/java/com/codecool/plaza/api/Shop.java
@@ -1,4 +1,28 @@
 package com.codecool.plaza.api;
 
+import java.util.List;
+
 public interface Shop {
+
+    public String getName();
+
+    public String getOwner();
+
+    public boolean isOpen();
+
+    public void open();
+
+    public void close();
+
+    public List<Product> findByName(String name) throws ShopIsClosedException;
+
+    public boolean hasProduct(long barcode) throws ShopIsClosedException;
+
+    public void addNewProduct(Product product, int quantity, float price) throws ProductAlreadyExistsException, ShopIsClosedException;
+
+    public void addProduct(long barcode, int quantity) throws NoSuchProductException, ShopIsClosedException;
+
+    public Product buyProduct(long barcode) throws NoSuchProductException, ShopIsClosedException;
+
+    public String toString();
 }
