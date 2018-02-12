7b6bb156fddb01663548066eb7dbc97dad2a7c84
Gyorfisti <gyorfisti@gmail.com>
2018-02-12 15:36:30 +0100

    completed Product class

--- src/main/java/com/codecool/plaza/api/Product.java
+++ src/main/java/com/codecool/plaza/api/Product.java
@@ -1,4 +1,24 @@
 package com.codecool.plaza.api;
 
 public abstract class Product {
+
+    protected long barcode;
+    protected String manufacturer;
+
+    protected Product(long barcode, String manufacturer) {
+        this.barcode = barcode;
+        this.manufacturer = manufacturer;
+    }
+
+    public long getBarcode() {
+        return barcode;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String toString() {
+        return null;
+    }
 }
